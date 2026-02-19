4981c2eaa6fd189e62b50defed6de1514577358a
Leo Lai <leo22lai@gmail.com>
2026-02-18 23:13:37 -0800

    minor update

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: process.env.PGUSER || 'leo22',
+  user: process.env.PGUSER || 'ben',
   host: process.env.PGHOST || 'localhost',
   database: process.env.PGDATABASE || 'bruin_match',
   password: process.env.PGPASSWORD || null,
